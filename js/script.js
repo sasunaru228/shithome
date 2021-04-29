59a2dabf75ec7b9e24588c0afbd2b8aaee9386d1
sasunaru228 <od26102000@gmail.com>
2021-04-29 13:43:04 +0300

    папа сделал нах

--- js/script.js
+++ js/script.js
@@ -6,17 +6,44 @@ $('#slider1').slick({
     fade: true,
     cssEase: 'linear'
 });
-document.getElementById('scroll').addEventListener('click', () => {
-    $('html, body').animate({
-        scrollTop: 800
-    }, 500);
-})
+if (document.getElementById('scroll') != null){
+    document.getElementById('scroll').addEventListener('click', () => {
+        $('html, body').animate({
+            scrollTop: 800
+        }, 500);
+    })
+}
 
-new Splide( '.splide', {
-    pagination: false,
-    type   : 'loop',
-    padding: {
-        right: '350px',
-        left : '350px',
-    },
-} ).mount();
+if (document.querySelector('.splide') != null){
+    new Splide( '.splide', {
+        pagination: false,
+        type   : 'loop',
+        padding: {
+            right: '350px',
+            left : '350px',
+        },
+    } ).mount();
+}
+
+
+
+if (document.querySelector('.modal') !== null){
+    let modal = new tingle.modal({})
+    document.querySelector(".modal").addEventListener('click', () => {
+        modal.open()
+    })
+    if (document.querySelector(".modal2") != null){
+        document.querySelector(".modal2").addEventListener('click', () => {
+            modal.open()
+        })
+    }
+    document.querySelector(".modal3").addEventListener('click', () => {
+        modal.open()
+    })
+    // document.querySelector("#modal_close").addEventListener('click', () => {
+    //     modal.close()
+    // })
+    let modal_content = document.querySelector('#modal_content');
+    modal.setContent(modal_content);
+}
+
