72f2a936c66a200e33125f9d7d7fef035c2fdb0c
sasunaru228 <od26102000@gmail.com>
2021-05-11 13:27:03 +0300

    mobile index

--- js/script.js
+++ js/script.js
@@ -13,15 +13,26 @@ if (document.getElementById('scroll') != null){
         }, 500);
     })
 }
+let width1, right1, left1;
+width1 = window.screen.width;
+if (width1 <= 1400){
+    right1 = '0';
+    left1 = '0';
+}
+else{
+    right1 = '350px';
+    left1 = '350px';
+}
 
 if (document.querySelector('.splide') != null){
     new Splide( '.splide', {
         pagination: false,
         type   : 'loop',
         padding: {
-            right: '350px',
-            left : '350px',
+            right: right1,
+            left : left1,
         },
+        focus: 'center',
     } ).mount();
 }
 
@@ -79,4 +90,45 @@ $("#6").click(function() { // ID откуда кливаем
         scrollTop: $(".contact-us").offset().top  // класс объекта к которому приезжаем
     }, 1000); // Скорость прокрутки
 });
+
+$("#11").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".projects").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+$("#22").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".technology").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#33").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".why").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#44").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".works").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#55").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".response").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+$("#66").click(function() { // ID откуда кливаем
+    $('html, body').animate({
+        scrollTop: $(".contact-us").offset().top  // класс объекта к которому приезжаем
+    }, 1000); // Скорость прокрутки
+});
+
+$('.navbar__burger').click(() => {
+    $('#nav-mobile').toggleClass('navbar--mobile--active')
+    $('.navbar__burger').toggleClass('navbar__burger--active')
+})
+$('#nav-mobile').click(() => {
+    $('#nav-mobile').removeClass('navbar--mobile--active')
+    $('.navbar__burger').removeClass('navbar__burger--active')
+})
 
